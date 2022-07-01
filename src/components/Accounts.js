70a0cca9957dc367e7643d37f2ee6f96cd00d014
972Olivier <oliviercen@gmail.com>
2022-07-01 09:11:58 -0400

    all components ok

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,14 +1,42 @@
+import Button from "./Button";
 const Accouts = ({ accounts }) => {
   const data = accounts;
   //   console.log(data);
   const operation = data.operations;
-  console.log(operation);
+  // console.log(operation);
   return (
     <section>
       <div className="name" style={{ backgroundColor: data.color }}>
         <h2>{data.name}</h2>
-        <span>{data.balance} €</span>
+        <span>
+          {`${data.balance.toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
+           €`}
+        </span>
       </div>
+      {operation.map((element, index) => {
+        let amount = element.amount.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        });
+        console.log(amount);
+        return (
+          <div className={`box${index}`}>
+            <span className="date">{element.date}</span>
+            <div className="twoSpan">
+              <div>
+                <span>{element.description}</span>
+              </div>
+              <div>
+                <span className="amount">{`${amount} €`}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <Button children="SEE MORE"></Button>
     </section>
   );
 };
